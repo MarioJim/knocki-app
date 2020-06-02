263be82a011783129c95153d73f7158e10ef8006
MarioJim <mario.emilio.j@gmail.com>
2020-06-02 01:31:27 -0500

    Added files from build

---
+++ precache-manifest.188849df223593daf4c18cad4c527e3f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a610b2a5e5836169287c99309e1408a",
+    "url": "/knocki-app/index.html"
+  },
+  {
+    "revision": "9d77dd4c355a789312e6",
+    "url": "/knocki-app/static/css/2.43a1393a.chunk.css"
+  },
+  {
+    "revision": "c90ac4b46e6c2b944026",
+    "url": "/knocki-app/static/css/main.c8164d45.chunk.css"
+  },
+  {
+    "revision": "9d77dd4c355a789312e6",
+    "url": "/knocki-app/static/js/2.f268c8de.chunk.js"
+  },
+  {
+    "revision": "c90ac4b46e6c2b944026",
+    "url": "/knocki-app/static/js/main.3566a5a4.chunk.js"
+  },
+  {
+    "revision": "22f31a2cab1a5b6cf7db",
+    "url": "/knocki-app/static/js/runtime~main.f97e9a91.js"
+  },
+  {
+    "revision": "62398b3a261f5e57dc9434745ffe2b12",
+    "url": "/knocki-app/static/media/add.62398b3a.svg"
+  },
+  {
+    "revision": "296a4b811e23f8489c42c6822ea69230",
+    "url": "/knocki-app/static/media/avatar.296a4b81.svg"
+  },
+  {
+    "revision": "8d60ec65b9560c7ea29d97c67e7f51a7",
+    "url": "/knocki-app/static/media/backspace-arrow.8d60ec65.svg"
+  },
+  {
+    "revision": "ad614847e1a74aa01ef439d79758cb3e",
+    "url": "/knocki-app/static/media/devices.ad614847.svg"
+  },
+  {
+    "revision": "04e7318dd951ad337147d1dd11943011",
+    "url": "/knocki-app/static/media/empty.04e7318d.svg"
+  },
+  {
+    "revision": "e8d47ac5e71daafce037d867779904a0",
+    "url": "/knocki-app/static/media/full.e8d47ac5.svg"
+  },
+  {
+    "revision": "4ae136b3114d8b3ce4e6e7c9a3bec4b9",
+    "url": "/knocki-app/static/media/gestures.4ae136b3.svg"
+  },
+  {
+    "revision": "edb3a3d3276006bd16b52cfe315abe96",
+    "url": "/knocki-app/static/media/location.edb3a3d3.svg"
+  },
+  {
+    "revision": "b8be9a60b42310f77d9439244572787a",
+    "url": "/knocki-app/static/media/logo.b8be9a60.svg"
+  },
+  {
+    "revision": "9a94afce141b8279b82bab3be8c93927",
+    "url": "/knocki-app/static/media/settings.9a94afce.svg"
+  },
+  {
+    "revision": "6698c65ab32a4fe7677c5ec9f1b54d9d",
+    "url": "/knocki-app/static/media/surface.6698c65a.svg"
+  },
+  {
+    "revision": "aeb2172cda54efa7d404bfd9ba4aa874",
+    "url": "/knocki-app/static/media/tag.aeb2172c.svg"
+  }
+]);
